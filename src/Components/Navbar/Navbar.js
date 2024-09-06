6548b78905b03645e7961cebaa034387c92f9c8d
SwayamprabhaGitHub <pswayamprabha@gmail.com>
2024-09-06 18:52:37 +0530

    added the logout functionality

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 import CartButton from "../Cart/CartButton";
 import "./Navbar.css";
 import Cart from "../Cart/Cart";
+import AuthContext from "../store/auth-context";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const authCtx = useContext(AuthContext);
   const [showCart, setShowCart] = useState(false);
 
   const showCartHandler = () => {
@@ -13,22 +16,46 @@ const Navbar = () => {
   return (
     <nav className="nav-bar">
       <div className="nav-bar-div">
-        <NavLink className={({isActive}) => isActive ? 'active' : ''} to="">
+        <NavLink className={({ isActive }) => (isActive ? "active" : "")} to="">
           Home
         </NavLink>
-        <NavLink className={({isActive}) => isActive ? 'active' : ''} to="store">
+        <NavLink
+          className={({ isActive }) => (isActive ? "active" : "")}
+          to="store"
+        >
           Store
         </NavLink>
-        <NavLink className={({isActive}) => isActive ? 'active' : ''} to="about">
+        <NavLink
+          className={({ isActive }) => (isActive ? "active" : "")}
+          to="about"
+        >
           About
         </NavLink>
-        <NavLink className={({isActive}) => isActive ? 'active' : ''} to="contactUs">
+        <NavLink
+          className={({ isActive }) => (isActive ? "active" : "")}
+          to="contactUs"
+        >
           Contact Us
         </NavLink>
       </div>
-      <NavLink className={({isActive}) => isActive ? 'active' : ''} to="login">
+      {!authCtx.isLoggedin && (
+        <NavLink
+          className={({ isActive }) => (isActive ? "active" : "")}
+          to="login"
+        >
           Login
         </NavLink>
+      )}
+      {authCtx.isLoggedin && (
+        <button
+          onClick={() => {
+            authCtx.logout();
+            navigate('/');
+          }}
+        >
+          LogOut
+        </button>
+      )}
       <CartButton onShowCart={showCartHandler} />
       {showCart && <Cart onShowCart={showCartHandler} />}
     </nav>
