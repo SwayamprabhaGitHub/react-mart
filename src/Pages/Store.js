96234e129dcdec05e4798ffe5265ac3602377b59
SwayamprabhaGitHub <pswayamprabha@gmail.com>
2024-09-06 19:51:26 +0530

    Protecting the store page

--- src/Pages/Store.js
+++ src/Pages/Store.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import Products from "../Components/Products/Products";
+import AuthContext from "../Components/store/auth-context";
+import { Navigate } from "react-router-dom";
 
 const Store = () => {
+  const authCtx = useContext(AuthContext);
+  if(!authCtx.isLoggedin) {
+    return <Navigate to="/login" replace />;
+  }
   return <main>
     <Products />
   </main>
